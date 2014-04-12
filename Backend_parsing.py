ac418cb917e6749705dd829e8e45d78fce361972
Rick <knoop.rick@gmail.com>
2014-04-12 15:08:33 -0400

    File Reading

--- Backend_parsing.py
+++ Backend_parsing.py
@@ -1,12 +1,18 @@
+import json
+
 class Data_Grabber:
     def __init__ (self):
         self.data = None
 
     def Read_file(self):
-        print "file"
+        with open("qlt5pn3.txt", "r") as fp:
+            json_Data = json.load(fp)
+            return json_Data
 
     def Request_json(self):
-        print "online"
+        print "json request"
+
+
 
 
 class Parser:
@@ -14,7 +20,13 @@ class Parser:
         self.Grab_data()
 
     def Grab_data(self):
-        print "grab"
+        the_Data = Data_Grabber().Read_file()
+        average_Q = 0
+        for element in the_Data:
+            average_Q += element["q"]
+        average_Q /= len(the_Data)-1
+        print average_Q
+
 
 
 if __name__ == "__main__":
