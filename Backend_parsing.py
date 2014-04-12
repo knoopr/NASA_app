ac1c1d7ddfeea60f1beff781bc4695d826b379a9
Rick <knoop.rick@gmail.com>
2014-04-12 17:30:42 -0400

    Formatting data for D3

--- Backend_parsing.py
+++ Backend_parsing.py
@@ -1,5 +1,6 @@
 import json
 from urllib2 import urlopen
+from re import match
 
 class Data_Grabber:
     def __init__ (self):
@@ -56,13 +57,92 @@ class Parser:
         self.Grab_data()
 
     def Grab_data(self):
-        the_Data = Data_Grabber().Request_json("q>=1")
-        """the_Data = Data_Grabber().Read_file()
-        average_Q = 0
+        #the_Data = Data_Grabber().Request_json("q>=1")
+        the_Data = Data_Grabber().Read_file()
+        """average_Q = 0
         for element in the_Data:
             average_Q += element["q"]
-        average_Q /= len(the_Data)-1
+        average_Q /= len(the_Data)
         print average_Q"""
+        self.config_Output(the_Data)
+
+    def config_Output(self, the_Data):
+        total_Q = 0
+        eighteenth_Century = []
+        nineteenth_Century = []
+        twentieth_Century = []
+        twentyfirst_Century = []
+        
+        json = """{
+                        "name":"NEO",
+                        "children"[
+                                {"""
+        
+        """
+        for element in the_Data:
+            if match("^17[0-9]{2}",element["first_obs"]) != None:
+                if element["spec"] in eighteenth_Century:
+                    eighteenth_Century[element["spec"]].append(element)
+                else:
+                    eighteenth_Century[element["spec"]] = []
+                    eighteenth_Century[element["spec"]].append(element)
+            elif match("^18[0-9]{2}",element["first_obs"]) != None:
+                if element["spec"] in nineteenth_Century:
+                    nineteenth_Century[element["spec"]].append(element)
+                else:
+                    nineteenth_Century[element["spec"]] = []
+                    nineteenth_Century[element["spec"]].append(element)
+            elif match("^19[0-9]{2}",element["first_obs"]) != None:
+                if element["spec"] in twentieth_Century:
+                    twentieth_Century[element["spec"]].append(element)
+                else:
+                    twentieth_Century[element["spec"]] = []
+                    twentieth_Century[element["spec"]].append(element)
+            elif match("^20[0-9]{2}",element["first_obs"]) != None:
+                if element["spec"] in twentyfirst_Century:
+                    twentyfirst_Century[element["spec"]].append(element)
+                else:
+                    twentyfirst_Century[element["spec"]] = []
+                    twentyfirst_Century[element["spec"]].append(element)"""
+
+
+        for element in the_Data:
+            if match("^17[0-9]{2}",element["first_obs"]) != None:
+                eighteenth_Century.append(element)
+            elif match("^18[0-9]{2}",element["first_obs"]) != None:
+                nineteenth_Century.append(element)
+            elif match("^19[0-9]{2}",element["first_obs"]) != None:
+                twentieth_Century.append(element)
+            elif match("^20[0-9]{2}",element["first_obs"]) != None:
+                twentyfirst_Century.append(element)
+
+
+
+        json += """"name":"18th Century",
+            "children":["""
+        for element in eighteenth_Century:
+        #json += """{"name":""" + '"' + element["name"] + '"' + ""","Perihelion":""" + '"' + element["q"]  + '"' """}"""
+            json += """{"name":"%s","Perihelion":"%s"}"""% (element["name"], element["q"])
+        json += ']}'
+        json += """"name":"19th Century",
+            "children":["""
+        for element in nineteenth_Century:
+            json += """{"name":"%s","Perihelion":"%s"}"""% (element["name"], element["q"])
+        json += ']}'
+        json += """"name":"20th Century",
+            "children":["""
+        for element in twentieth_Century:
+            json += """{"name":"%s","Perihelion":"%s"}"""% (element["name"], element["q"])
+        json += ']}'
+        json += """"name":"21st Century",
+            "children":["""
+        for element in twentyfirst_Century:
+            json += """{"name":"%s","Perihelion":"%s"}"""% (element["name"], element["q"])
+        json += ']}]}}'
+
+        print json
+
+
 
 
 
