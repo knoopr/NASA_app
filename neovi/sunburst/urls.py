3c1c74dc487ce3691e49a42c8348fb33e77131e4
Reid Miller <r24mille@uwaterloo.ca>
2014-04-12 15:51:08 -0400

    Migrated JSON parsing into a django view so that it can be ready by javascript.

--- neovi/sunburst/urls.py
+++ neovi/sunburst/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from sunburst import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^asterank_json$', views.asterank_json, name='asterank_json')
 ]
--- neovi/sunburst/views.py
+++ neovi/sunburst/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import json
 
 def index(request):
     text_for_page = "Text from view code"
@@ -7,3 +8,8 @@ def index(request):
         'text_for_page': text_for_page,
     }
     return render(request, 'sunburst/index.html', context)
+
+def asterank_json(request):
+    with open("sunburst/static/sunburst/txt/qlt5pn3.txt", "r") as fp:
+        json_Data = json.load(fp)
+        return HttpResponse(json_Data, mimetype='application/json')
