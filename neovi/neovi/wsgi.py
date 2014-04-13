deb7c9cba16656097e25c2df7e5f1fea14f2c6d3
Reid Miller <r24mille@uwaterloo.ca>
2014-04-13 02:12:22 -0400

    Completing recommendations from https://devcenter.heroku.com/articles/django-assets

--- neovi/neovi/wsgi.py
+++ neovi/neovi/wsgi.py
@@ -11,4 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "neovi.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+# Changing as recommended by https://devcenter.heroku.com/articles/django-assets
+# application = get_wsgi_application()
+application = Cling(get_wsgi_application())
