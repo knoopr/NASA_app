e9208a64e6ecc7f4070e6aa8145fc2b8be336ea0
Reid Miller <r24mille@uwaterloo.ca>
2014-04-13 08:55:22 -0400

    Moved the sunburst index to neovi/urls.py so that it can map to site root.

--- neovi/neovi/urls.py
+++ neovi/neovi/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from sunburst import views as sbv
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'neovi.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', sbv.index, name='sunburst.index'),
     url(r'^sunburst/', include('sunburst.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- neovi/sunburst/urls.py
+++ neovi/sunburst/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from sunburst import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # Moving to ../neovi/urls.py so that index can live at site root
+    # url(r'^$', views.index, name='index'),
     url(r'^asterank_json$', views.asterank_json, name='asterank_json')
 ]
